8377173493d8ed30a6b3e03561c6f2a93c866bda
Huu Tu <huutu1368@gmail.com>
2026-03-17 21:12:20 +0700

    NguyenHuuTunopbaitap1

---
+++ NguyenHuuTu-Bai1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Bai1
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Nguyen Huu Tu // 2425053122346");
+        Console.WriteLine("Bai 1");
+        List<int> list1 = new List<int>();
+        list1.Add(1);
+        list1.Add(2);
+        list1.Add(3);
+        list1.Add(4);
+        list1.Add(5);
+        Console.WriteLine(" Danh sach cac phan tu:");
+        foreach (var pt in list1)
+        {
+            Console.WriteLine(pt);
+        }
+    }
+}
