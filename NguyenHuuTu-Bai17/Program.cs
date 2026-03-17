f163568ba5e647f198f687759f7dedb7dec8926c
Huu Tu <huutu1368@gmail.com>
2026-03-17 21:57:28 +0700

    NguyenHuuTunopbaitap17

---
+++ NguyenHuuTu-Bai17/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+class Bai17
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nguyen Huu Tu //  2415053122346");
+        List<int> list17 = new List<int>();
+        Console.WriteLine("Nhap vao so luong phan tu:");
+        int.TryParse(Console.ReadLine(), out int n);
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Nhap vao phan tu thu {i + 1}:");
+            int.TryParse(Console.ReadLine(), out int pt);
+            list17.Add(pt);
+        }
+        int maxCount = 0;
+        int ptnhieu = list17[0];
+        foreach (int pt in list17)
+        {
+            int count = 0;
+            foreach (int item in list17)
+            {
+                if (item == pt)
+                    count++;
+            }
+            if (count > maxCount)
+            {
+                maxCount = count;
+                ptnhieu = pt;
+            }
+        }
+        Console.WriteLine("So xuat hien nhieu nhat: " + ptnhieu);
+        Console.WriteLine("So lan xuat hien: " + maxCount);
+    }
+}
