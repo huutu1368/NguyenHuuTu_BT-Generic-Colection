a5ee769703da48e666ef7c4fa7f38dde13702be4
Huu Tu <huutu1368@gmail.com>
2026-03-17 21:55:20 +0700

    NguyenHuuTunopbaitap16

---
+++ NguyenHuuTu-Bai16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Bai16
+{
+    static void Main()
+    {
+        Console.WriteLine("Nguyen Huu Tu //  2415053122346");
+        Console.Write("Nhap chuoi: ");
+        string input = Console.ReadLine();
+        Dictionary<char, int> dem = new Dictionary<char, int>();
+        foreach (char c in input)
+        {
+            if (dem.ContainsKey(c))
+                dem[c] = dem[c] + 1;
+            else
+                dem[c] = 1;
+        }
+        Console.WriteLine("\nKet qua:");
+        foreach (var item in dem)
+        {
+            Console.WriteLine($"{item.Key} : {item.Value}");
+        }
+    }
+}
