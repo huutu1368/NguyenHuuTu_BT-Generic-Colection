dda0d9c5bc18d0c44433f9ed4c885827b89f0c87
Huu Tu <huutu1368@gmail.com>
2026-03-17 21:23:28 +0700

    NguyenHuuTunopbaitap3

---
+++ NguyenHuuTu-Bai3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+class bai3
+{
+    public static void  Main(String[] args)
+    {
+        Console.WriteLine("Nguyen Huu Tu // 2415053122346");
+        List<int> list3 = new List<int>();
+        Console.WriteLine("nhap so luong phan tu:");
+        int.TryParse(Console.ReadLine(), out var n);
+        if (n<0)
+        {
+            Console.WriteLine("danh sach trong, khong co so lon nhat");
+            return;
+        }
+        for(int i=0; i < n; i++)
+        {
+            Console.WriteLine($"Nhap phan tu thu {i+1}");
+            int.TryParse(Console.ReadLine(), out var pt);
+            list3.Add(pt);
+        }
+        int max = list3[0];
+        foreach ( var pt in list3)
+        {
+            if(pt> max)
+            {
+                max = pt;
+            }
+        }
+        Console.WriteLine($"so lon nhat trong danh sach la: {max}");
+    }
+}
